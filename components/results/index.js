b928c8c04961fa97bc740cf1bf10b44fa712c0d6
Diego Kfuri <kkfuri@gmail.com>
2020-05-17 18:59:13 -0300

    feat: create results container with animation

---
+++ components/results/index.js
@@ -0,0 +1,36 @@
+import { motion } from "framer-motion";
+import ListItem from "../list-item";
+
+const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const itemMotion = {
+  hidden: { opacity: 0, y: 30 },
+  show: { opacity: 1, y: 0 },
+};
+
+function Results({ list }) {
+  return (
+    <motion.div
+      variants={container}
+      initial="hidden"
+      animate="show"
+      className="grid grid-cols-4 gap-8 py-12"
+    >
+      {list.map((item) => (
+        <motion.div key={item.id} variants={itemMotion}>
+          <ListItem item={item} />
+        </motion.div>
+      ))}
+    </motion.div>
+  );
+}
+
+export default Results;
