cb63e0079b702ff3d6fa3f523e212814eca1ac98
Diego Kfuri <kkfuri@gmail.com>
2020-05-17 00:47:49 -0300

    feat: create logo component

---
+++ components/logo/index.js
@@ -0,0 +1,9 @@
+function Logo() {
+  return (
+    <h1 className="duration-200 inline-block m-8 text-xl absolut inset-0 p-8 hover:text-primary-dark cursor-default hover:shadow-xl">
+      Superhero
+    </h1>
+  );
+}
+
+export default Logo;
