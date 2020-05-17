2a605d61385a95ac5b0134e9903f4a3d873d2b0a
Diego Kfuri <kkfuri@gmail.com>
2020-05-17 00:48:29 -0300

    feat: create homepage with logo and search container

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
+import Search from "@/components/search";
+import Logo from "@/components/logo";
+
 function Home() {
+  function handleSearch(value) {
+    if (value) {
+      alert(value);
+    }
+  }
+
   return (
-    <div>
-      <h2>Superhero</h2>
-    </div>
+    <main className="bg-primary-light min-h-screen">
+      <Logo />
+      <div className="mx-auto container">
+        <div className="flex flex-col items-center">
+          <Search onSearch={handleSearch} />
+        </div>
+      </div>
+    </main>
   );
 }
 
