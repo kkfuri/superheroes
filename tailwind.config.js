960e6ec658db5a4af7bed94920e8cbc3c2a9ad4d
Diego Kfuri <kkfuri@gmail.com>
2020-05-17 00:08:06 -0300

    feat: setup initial styles and import google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   purge: ["./components/**/*.js", "./pages/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        typography: "#232323",
+        primary: {
+          default: "#2980b9",
+          dark: "#2c3e50",
+          light: "#3498db",
+        },
+      },
+    },
+    fontFamily: {
+      display: ["Balsamiq Sans", "cursive"],
+      body: ["Source Sans Pro", "sans-serif"],
+    },
   },
   variants: {},
   plugins: [],
