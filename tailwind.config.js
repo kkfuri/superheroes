d4cd86109191a047d39edaf803f142af90de381f
Diego Kfuri <kkfuri@gmail.com>
2020-05-17 18:58:37 -0300

    feat: add framer motion, body scroll lock and integrate spinner to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   purge: ["./components/**/*.js", "./pages/**/*.js"],
   theme: {
+    spinner: (theme) => ({
+      default: {
+        color: "#2c3e50", // color you want to make the spinner
+        size: "8em", // size of the spinner (used for both width and height)
+        border: "2px", // border-width of the spinner (shouldn't be bigger than half the spinner's size)
+        speed: "500ms", // the speed at which the spinner should rotate
+      },
+    }),
     extend: {
       colors: {
         typography: "#232323",
@@ -9,6 +17,7 @@ module.exports = {
           dark: "#2c3e50",
           light: "#3498db",
         },
+        overlay: "rgba(0,0,0, 0.99)",
       },
     },
     fontFamily: {
@@ -19,6 +28,7 @@ module.exports = {
   variants: {
     textColor: ["responsive", "hover", "focus", "group-hover"],
     scale: ["responsive", "hover", "focus", "active", "group-hover"],
+    spinner: ["responsive"],
   },
-  plugins: [],
+  plugins: [require("tailwindcss-spinner")()],
 };
