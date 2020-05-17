a585b0eaddd3c526141ded530276792138410111
Diego Kfuri <kkfuri@gmail.com>
2020-05-17 19:49:55 -0300

    fix: adjust api url usage

--- utils/api.js
+++ utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `https://superheroapi.com/api/${process.env.API_KEY}`,
+  baseURL: `https://superheroapi.com/api.php/${process.env.API_KEY}`,
 });
