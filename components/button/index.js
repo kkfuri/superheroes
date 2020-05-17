1503aac8ceb6bfe62ad376a0eabc30edd05f17c6
Diego Kfuri <kkfuri@gmail.com>
2020-05-17 20:05:57 -0300

    feat: animate button

--- components/button/index.js
+++ components/button/index.js
@@ -1,9 +1,19 @@
+import { motion } from "framer-motion";
+
 function Button({ type, className, ...props }) {
   const classNames = [
-    "w-full p-4 shadow-sm duration-200 rounded-sm bg-transparent border border-primary-light hover:shadow-lg uppercase",
+    "w-full p-4 duration-200 shadow-md rounded-sm bg-transparent border border-primary-light hover:shadow-xl uppercase",
     className,
   ].join(" ");
-  return <button type={type} className={classNames} {...props} />;
+  return (
+    <motion.button
+      whileHover={{ scale: 1.02 }}
+      whileTap={{ scale: 0.98 }}
+      type={type}
+      className={classNames}
+      {...props}
+    />
+  );
 }
 
 export default Button;
