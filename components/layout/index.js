da6cf25316ea15eaec51df1d743b9dcbf2fbe450
Diego Kfuri <kkfuri@gmail.com>
2020-06-05 22:42:43 -0300

    feat: Add icon and link to repo and add animation to footer links

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,27 +1,45 @@
+import { FaGithub } from "react-icons/fa";
+import { motion } from "framer-motion";
+
 function Layout({ children }) {
   return (
     <div className="flex flex-col min-h-screen bg-primary-light">
+      <motion.a
+        whileHover={{ scale: 1.2 }}
+        target="_blank"
+        rel="noopener"
+        href="https://github.com/kkfuri/superheroes"
+        aria-label="Go to repository"
+        className="absolute top-0 right-0 mt-8 mr-8 text-4xl"
+      >
+        <FaGithub />
+      </motion.a>
+
       <main className="flex flex-col flex-1 h-full">{children}</main>
       <footer className="w-full mt-8 mb-4 text-center">
-        <code>
-          Made by{" "}
-          <a
+        <code className="flex flex-row items-center justify-center">
+          <span>Made by </span>
+          <motion.a
+            whileHover={{ y: -4 }}
             target="_blank"
             rel="noopener"
             href="https://github.com/kkfuri"
             aria-label="Go to dev github"
+            className="mx-2"
           >
             @kkfuri
-          </a>{" "}
-          with{" "}
-          <a
+          </motion.a>{" "}
+          <span>with</span>
+          <motion.a
+            whileHover={{ y: -4 }}
             target="_blank"
             rel="noopener"
             href="https://nextjs.org/"
             aria-label="Go to nextjs page"
+            className="mx-2"
           >
             Next.js
-          </a>
+          </motion.a>
         </code>
       </footer>
     </div>
