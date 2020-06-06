ff0e3b19f41f591c47f8c5be32ac5f0ce14f15d8
Diego Kfuri <kkfuri@gmail.com>
2020-06-05 22:25:50 -0300

    fix: Fix logo name

--- components/logo/index.js
+++ components/logo/index.js
@@ -4,7 +4,7 @@ function Logo() {
   return (
     <Link href="/">
       <h1 className="inset-0 inline-block w-full p-8 text-xl text-center duration-200 cursor-default cursor-pointer md:m-8 md:max-w-xs md:w-auto hover:text-primary-dark hover:shadow-xl">
-        Superhero
+        Superheroes
       </h1>
     </Link>
   );
